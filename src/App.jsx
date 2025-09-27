2903a91e42192f3fa38f9396609a3c1509382587
Shubham1823L <shubhuisbetter@gmail.com>
2025-09-27 11:36:51 +0530

    Navbar added(mostly)

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './index.css'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+
 
 function App() {
   return (
     <>
+      <Navbar />
+      <div className='heroWrapper'>
+        {/* Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint cupiditate qui ab voluptates voluptate, aspernatur repudiandae voluptatem enim laudantium nam a mollitia magnam tenetur distinctio! Error voluptatem minima possimus sequi */}
+        <Sidebar />
+      </div>
 
     </>
   )
